615b3e185059a2dea663bff12de6f1009defb80e
felix.ng <nfelix@gmail.com>
2017-10-27 15:04:40 +0100

    Update title for ben

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">InstaProgress</h1>
+          <h1 className="App-title">Happy Birthday! (This is work in progress...)</h1>
           <Button
                 bsStyle="primary"
                 className="btn-margin"
