303fbe2cb68b55fc780e2979fa1b755ff1f7a983
felix.ng <nfelix@gmail.com>
2017-10-29 14:08:01 +0000

    font alignment

--- src/Footer.js
+++ src/Footer.js
@@ -4,28 +4,47 @@ import { Link } from 'react-router-dom';
 import './App.css';
 
 class Footer extends Component {
-  goTo(route) {
-    this.props.history.replace(`/${route}`)
-  }
-
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
-
   render() {
     const { isAuthenticated } = this.props.auth;
 
     return (
       <footer className="footer">
-        Link
-        Link
-        Link
-        Link
-      </footer>
+        {
+          isAuthenticated() && (
+              <div className="instagram-footer">
+                <div className="totalSessions">
+                  <div className="sub-heading">
+                    Total sessions
+                  </div>
+                  <div className="heading">
+                    X
+                  </div>
+                  <div className="sub-heading">
+                    +/- from last week
+                  </div>
+                </div>
+                <div className="weeklyActivity">
+                  <div className="sub-heading">
+                    Weekly activity
+                  </div>
+                  <div className="heading">
+                    XX% active
+                  </div>
+                </div>
+              </div>
+            )
+        }
+        {
+          !isAuthenticated() && (
+              <div className="footer">
+                Link
+                Link
+                Link
+                Link
+              </div>
+            )
+        }
+        </footer>
     );
   }
 }
