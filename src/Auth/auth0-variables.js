037c328241ea8fe2b40795b8f5114a089fcc0e60
felix.ng <nfelix@gmail.com>
2017-12-02 16:48:03 +0000

    retry process.env

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export var AUTH_CONFIG = {
   domain: 'felixng.eu.auth0.com',
   clientId: 'yh9KjUp5eCAJjuqzT1t1xB5KjrbRv5dG',
-  callbackUrl: 'http://itsbensbirthdayomg.herokuapp.com/callback'
+  callbackUrl: process.env.AUTH0_DOMAIN || 'http://localhost:3000/callback'
 }
