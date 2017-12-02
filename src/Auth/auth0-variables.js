9cc7e97dcddf28ac84eaaee2e3a528887dbb0f32
felix.ng <nfelix@gmail.com>
2017-12-02 16:51:34 +0000

    changing back to static callback

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export var AUTH_CONFIG = {
   domain: 'felixng.eu.auth0.com',
   clientId: 'yh9KjUp5eCAJjuqzT1t1xB5KjrbRv5dG',
-  callbackUrl: process.env.AUTH0_DOMAIN || 'http://localhost:3000/callback'
+  callbackUrl: 'http://itsbensbirthdayomg.herokuapp.com/callback/'
 }
